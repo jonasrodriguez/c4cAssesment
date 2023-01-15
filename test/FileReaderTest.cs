0bcb97c951ec5f686770685ee0f504b2652066d3
Jonas Rodriguez <jonas.rodriguez_murillo@emea.roche.com>
2023-01-15 17:56:11 +0100

    Improved file reader test.

--- test/FileReaderTest.cs
+++ test/FileReaderTest.cs
@@ -3,14 +3,27 @@ namespace test;
 public class FileReaderTest {
 
     private static readonly string FILE_PATH = "./resources" ;
+    private static readonly string WRONG_PATH = "./resources2" ;
+
+    [Fact]
+    public void ProcessFolderFilesShould_returnFilesOnPath_AndContent() {
+        FileReader reader = new FileReader();
+        var content = reader.ProcessFolderFiles(FILE_PATH);
+        Assert.Equal(2, content.Count);
+
+        string [] files = Directory.GetFiles(FILE_PATH);
+        foreach(string f in files) {
+            string filename = Path.GetFileName(f);
+            String fileContent = File.ReadAllText(f);
+            Assert.True(content.ContainsKey (filename));
+            Assert.Equal(fileContent, content[filename]);
+        }
+    }    
 
     [Fact]
     public void ProcessFolderFilesShould_returnEmptyList_whenPathNotExist() {
-        string path = Directory.GetCurrentDirectory();
-
         FileReader reader = new FileReader();
-        var content = reader.ProcessFolderFiles(FILE_PATH);
+        var content = reader.ProcessFolderFiles(WRONG_PATH);
         Assert.Empty(content);
     }
-
 }
